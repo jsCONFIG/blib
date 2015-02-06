866dbc381615e6891158e62a4f5126f35d03f798
jsCONFIG <clpliuping@126.com>
2015-02-06 18:49:07 +0800

    fix bug

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var _get = function ( id ) {
     return document.getElementById( id );
 };
+
+var _stringfy = function ( obj ) {
+    return window.JSON ? JSON.stringify( obj ) : ( obj + '' );
+}
 requirejs.config({
     baseUrl: 'lib'
 });
